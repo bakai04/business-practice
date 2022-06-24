3fa54f3f3e0a595bb7d651a714ad2a3b86f6a71f
bakai04 <joldoshovbakai04@gmail.com>
2022-06-24 18:49:58 +0600

    improv bags

--- src/js/modules/getLocation.js
+++ src/js/modules/getLocation.js
@@ -1,6 +1,5 @@
 function getLocation() {
   const links = document.querySelectorAll(".menu__link");
-  const trainingCard = document.querySelector(".training__links");
 
   if(window.location.href.indexOf("/aboutTraining.html")!=(-1)){
     links[2].classList.add("menu__link--active");
