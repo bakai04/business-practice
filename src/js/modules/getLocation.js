a6ed3c2b5738563f31c05228508d0d27b64656ae
bakai04 <joldoshovbakai04@gmail.com>
2022-06-24 20:53:23 +0600

    last commit

--- src/js/modules/getLocation.js
+++ src/js/modules/getLocation.js
@@ -1,9 +1,14 @@
 function getLocation() {
   const links = document.querySelectorAll(".menu__link");
 
-  if(window.location.href.indexOf("/aboutTraining.html")!=(-1)){
-    links[2].classList.add("menu__link--active")
-    links[5].classList.add("menu__link--active")
+  if (window.location.href.indexOf("/aboutTraining.html") != -1) {
+    links.forEach((element) => {
+      if (element.textContent === "Треннинги") {
+        element.classList.add("menu__link--active");
+      }
+    });
+    // links[2].classList.add("menu__link--active")
+    // links[5].classList.add("menu__link--active")
   }
   links.forEach((element) => {
     if (element.href == window.location.href) {
